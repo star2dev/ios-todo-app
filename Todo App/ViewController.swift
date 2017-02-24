17e7cc03693ed5d0b90213c985f837c7759f6b68
Bruce Galpin <bruce.galpin@arqino.com>
2017-02-24 08:47:29 +0200

    Initial Commit

---
+++ Todo App/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Todo App
+//
+//  Created by Bruce Galpin on 2017/02/24.
+//  Copyright © 2017 Social-IT-e Media. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
