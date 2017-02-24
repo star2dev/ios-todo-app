bf9775e3ea1c0576e35b4a02aa7e5c67104b5f08
Bruce Galpin <bruce.galpin@arqino.com>
2017-02-24 11:28:51 +0200

    Added view controllers

---
+++ Todo App/CreateTaskViewController.swift
@@ -0,0 +1,26 @@
+//
+//  CreateTaskViewController.swift
+//  Todo App
+//
+//  Created by Bruce Galpin on 2017/02/24.
+//  Copyright © 2017 Social-IT-e Media. All rights reserved.
+//
+
+import UIKit
+
+class CreateTaskViewController: UIViewController {
+
+    
+    @IBOutlet weak var taskNameTxt: UITextField!
+    @IBOutlet weak var taskFlagSwitch: UISwitch!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func addTaskBtn(_ sender: Any) {
+    }
+
+}
--- Todo App/ViewController.swift
+++ Todo App/ViewController.swift
@@ -8,18 +8,57 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    @IBOutlet weak var tableView1: UITableView!
+    
+    var tasks : [Task] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        tasks = makeTasks()
+        
+        tableView1.dataSource = self
+        tableView1.delegate = self
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return tasks.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        let task = tasks[indexPath.row]
+        
+        if (task.important) {
+            cell.textLabel?.text = "❗️ \(task.name)"
+        } else {
+            cell.textLabel?.text = "\(task.name)"
+        }
+        
+        return cell
+    }
+    
+    func makeTasks() -> [Task] {
+        let task1 = Task()
+        task1.name = "Don't walk the dog"
+        task1.important = false
+        
+        let task2 = Task()
+        task2.name = "Make coffee"
+        task2.important = true
+        
+        let task3 = Task()
+        task3.name = "Feed the cat"
+        task3.important = false
+        
+        return [task1, task2, task3]
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func plusTapped(_ sender: Any) {
+        performSegue(withIdentifier: "addSegue", sender: nil)
     }
-
-
 }
 
